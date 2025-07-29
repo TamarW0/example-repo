17af124607ff06fdf95734419b7ecd3af769c2f8
Tamar Weisskopf <tweissko@redhat.com>
2025-07-30 01:28:00 +0300

    Add use of werkzeug,formparser.MultiPartParser

--- src/example_main.py
+++ src/example_main.py
@@ -1,3 +1,7 @@
 from flask import helpers
-# change
+from werkzeug.formparser import MultiPartParser
+
 helpers.redirect('/home', 302)
+
+multi_part_parser = MultiPartParser()
+print(multi_part_parser.max_form_parts)
